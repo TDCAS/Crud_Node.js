2c59b4431c53747802aac23df239d09fe9ed2a84
TDCAS <allanf333@gmail.com>
2022-10-24 23:53:52 -0300

    principal2

--- src/index.mjs
+++ src/index.mjs
@@ -15,7 +15,7 @@ app.get("/",function(req,res){
 })
 
 app.post("/cadastrocomsucesso",function(req,res){
-    user.db_clinte(+req.body.cl_name,req.body.cl_email,req.body.cl_senha)
+    user.db_clinte(req.body.cl_name,req.body.cl_email,req.body.cl_senha)
     let senha = "*".repeat((req.body.cl_senha).length)
     res.send("Nome: "+req.body.cl_name+"<hr>Email:"+req.body.cl_email+"<hr>Senha:"+senha)
   
